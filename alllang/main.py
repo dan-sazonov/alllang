b0fe46c04fa17f36c7230941773ed362b9a82dd5
Dan Sazonov <p.294803@gmail.com>
2023-10-12 00:15:30 +0300

    Replace all with class

--- alllang/main.py
+++ alllang/main.py
@@ -5,14 +5,13 @@ from deep_translator import GoogleTranslator
 _lang_list = list(GoogleTranslator().get_supported_languages())
 
 
-def file_to_list(path: str) -> list[str]:
+def _file_to_list(path) -> list[str]:
     """
     Converts a txt file with words on separate lines to a list of strings
 
     :param path: absolute or relative path to the txt file
     :return: list of str
     """
-    _check_path(path)
 
     with open(path, 'r', encoding='utf-8') as f:
         file_content = f.read().split('\n')
@@ -21,34 +20,39 @@ def file_to_list(path: str) -> list[str]:
     return words
 
 
-def all_lang_trans(word: str, lang_list=None) -> dict[str, str]:
-    """
-    Translates the `word` into all languages
+class Translator:
+    def __init__(self):
+        self.path_to_file = None
+        self.lang_list = _lang_list
 
-    :param lang_list: list[str]. Languages into which the word will be translated. By default, all supported languages
-    :param word: the word to be translated
-    :return: dict where the key is the name of the language, the value is the translation
-    """
-    if lang_list is None:
-        lang_list = _lang_list
+    def set_file(self, path):
+        _check_path(path)
+        self.path_to_file = path
 
-    out = dict()
-    for lang in lang_list:
-        my_translator = GoogleTranslator(source='auto', target=lang)
-        result = my_translator.translate(text=word)
-        out[lang] = result
+    def set_lang_list(self, lang_list):
+        self.lang_list = lang_list
 
-    return out
+    def all_lang_trans(self, word: str) -> dict[str, str]:
+        """
+        Translates the `word` into all languages
 
+        :param lang_list: list[str]. Languages into which the word will be translated. By default, all supported languages
+        :param word: the word to be translated
+        :return: dict where the key is the name of the language, the value is the translation
+        """
+        out = dict()
+        for lang in self.lang_list:
+            my_translator = GoogleTranslator(source='auto', target=lang)
+            result = my_translator.translate(text=word)
+            out[lang] = result
 
-def transl_from_file(path: str, lang_list=None):
-    if lang_list is None:
-        lang_list = _lang_list
+        return out
 
-    word_list = file_to_list(path)
-    out = dict()
+    def transl_from_file(self):
+        word_list = _file_to_list(self.path_to_file)
+        out = dict()
 
-    for word in word_list:
-        out[word] = all_lang_trans(word, lang_list=lang_list)
+        for word in word_list:
+            out[word] = self.all_lang_trans(word=word)
 
-    return out
+        return out
