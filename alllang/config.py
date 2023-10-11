87b2aedfbf0e1a9bd687e805a85c2e75b13635f7
Dan Sazonov <p.294803@gmail.com>
2023-10-11 22:04:56 +0300

    Warn if non-txt file

--- alllang/config.py
+++ alllang/config.py
@@ -7,5 +7,6 @@ class ColorMethods:
         self.reset = colorama.Style.RESET_ALL
         self.red = colorama.Fore.RED
         self.green = colorama.Fore.GREEN
+        self.yellow = colorama.Fore.YELLOW
         self.bright = colorama.Style.BRIGHT
         self.dim = colorama.Style.DIM
--- alllang/main.py
+++ alllang/main.py
@@ -6,14 +6,17 @@ _c = config.ColorMethods()
 
 def file_to_list(path: str) -> list[str]:
     """
-    Converts a file with words on separate lines to a list of strings
+    Converts a txt file with words on separate lines to a list of strings
 
-    :param path: absolute or relative path to the file
+    :param path: absolute or relative path to the txt file
     :return: list of str
     """
 
     if (not os.path.exists(path)) or (not os.path.isfile(path)):
         print(f"{_c.red}[err]{_c.reset} Path of the file is Invalid")
+
+    if not path.endswith('.txt'):
+        print(f"{_c.yellow}[warn]{_c.reset} The path does not go to a text file, encoding error is possible")
 
     with open(path, 'r', encoding='utf-8') as f:
         file_content = f.read().split('\n')
