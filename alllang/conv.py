06223be1a23945db98a6e8bb67a094b276121d55
Dan Sazonov <p.294803@gmail.com>
2023-10-12 03:38:34 +0300

    Path could be None

--- alllang/conv.py
+++ alllang/conv.py
@@ -1,9 +1,10 @@
+import json
 import os
 from datetime import datetime
 
 
 def _get_file_name(path, ext):
-    if not os.path.exists(path):
+    if (path is None) or (not os.path.exists(path)):
         path = './'
     file_name = f"{datetime.today().isoformat('-').replace(':', '-').split('.')[0]} .{ext}"
 
