f470db7fbee51dcac8a625dbda73663839eda9ea
Dan Sazonov <p.294803@gmail.com>
2023-10-12 19:20:47 +0300

    Xlsx func sample

--- alllang/conv.py
+++ alllang/conv.py
@@ -2,6 +2,8 @@ import json
 import os
 from datetime import datetime
 
+import openpyxl
+
 
 def _get_file_name(path, ext):
     if (path is None) or (not os.path.exists(path)):
@@ -23,3 +25,17 @@ def create_json(data, dest_path=None):
 
     with open(file_name, 'w', encoding='utf-8') as json_file:
         json.dump(data, json_file, ensure_ascii=False)
+
+
+def create_xlsx(data, dest_path=None):
+    file_name = _get_file_name(dest_path, 'xlsx')
+
+    book = openpyxl.Workbook()
+    sheet = book.active
+
+    sheet[1][0].value = 10
+
+    book.save(file_name)
+    book.close()
+
+    print(_get_table_titles(data))
