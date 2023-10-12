600ed35a19a0aec86b393f1f74962c8ff4f92dfe
Dan Sazonov <p.294803@gmail.com>
2023-10-12 03:45:07 +0300

    Create json file from dict func

--- alllang/conv.py
+++ alllang/conv.py
@@ -10,3 +10,10 @@ def _get_file_name(path, ext):
 
     res = os.path.join(path, file_name)
     return os.path.normpath(res)
+
+
+def create_json(data, dest_path=None):
+    file_name = _get_file_name(dest_path, 'json')
+
+    with open(file_name, 'w', encoding='utf-8') as json_file:
+        json.dump(data, json_file, ensure_ascii=False)
