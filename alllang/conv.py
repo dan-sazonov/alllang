1c2c685da943b56e95ebfadd0882bba3c338da9d
Dan Sazonov <p.294803@gmail.com>
2023-10-12 03:25:54 +0300

    Create module for out files

---
+++ alllang/conv.py
@@ -0,0 +1,11 @@
+import os
+from datetime import datetime
+
+
+def _get_file_name(path, ext):
+    if not os.path.exists(path):
+        path = './'
+    file_name = f"{datetime.today().isoformat('-').replace(':', '-').split('.')[0]} .{ext}"
+
+    res = os.path.join(path, file_name)
+    return os.path.normpath(res)
